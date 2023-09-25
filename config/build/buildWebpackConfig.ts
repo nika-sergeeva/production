4a785562fa60037d01b772d75d787702d98ba515
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-09-25 23:43:34 +0300

    Webpack-dev-server. Переменные окружения (env)

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,22 +3,24 @@ import webpack from 'webpack'
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options :BuildOptions): webpack.Configuration {
-    const {paths, mode} = options
+    const {paths, mode, isDev} = options
  return {
     mode,
     entry: paths.entry,
-    module: {
-        rules: buildLoaders(),
-      },
-      resolve: buildResolvers(),
     output: {
         path: paths.build,
         filename: "[name].[contenthash].bundle.js",
         clean: true
     },
-    plugins: buildPlugins(options),
-
+    plugins: buildPlugins(options), 
+    module: {
+        rules: buildLoaders(),
+      },
+      resolve: buildResolvers(),
+      devtool: isDev? 'inline-source-map' : undefined,
+      devServer: buildDevServer(options)
 }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from "./test";
 
 someFn(111)
+
+document.body.innerHTML=   `<div>Hi thete!</div>`
