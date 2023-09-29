ca8e8c95e28fbdbab736a4367eb7f66c64f103fb
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-09-29 21:49:41 +0300

    Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { Counter } from './components/Counter'
+import { Routes, Route } from 'react-router-dom'
 import './index.scss'
+import { Link } from 'react-router-dom'
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
+import { Suspense } from 'react'
 
 const App = () => {
   return (
     <div className='app'>
-        dghrhgs
-      <Counter />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          
+            <Route path={'/about'} element={<AboutPageLazy />}/>
+            <Route path={'/'} element={<MainPageLazy />}/>
+          
+        </Routes>
+        </Suspense>
     </div>
   )
 }
