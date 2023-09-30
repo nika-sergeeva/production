43cbcf6a626ba16e47b9d2f7f597a89e1e9e9df1
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-09-30 23:18:29 +0300

    themes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Counter } from './components/Counter'
 import { Routes, Route } from 'react-router-dom'
-import './index.scss'
+import './styles/index.scss'
 import { Link } from 'react-router-dom'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 import { Suspense } from 'react'
+import { useTheme } from './theme/useTheme'
+
+
 
 const App = () => {
+ const {theme, toggleTheme} = useTheme()
+ 
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
