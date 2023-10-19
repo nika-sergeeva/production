9551f443db5a29a4641c55238171d61bb0c1baa8
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-10-20 00:04:53 +0300

    Navbar. First UI kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
-import { Suspense } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNmes'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/router/ined'
+import { Navbar } from 'widgets/Navbar'
 
 
 const App = () => {
@@ -12,8 +11,7 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
+      <Navbar />
       <AppRouter />
     </div>
   )
