1e08fd93f5e4fa674d979bcd1062d9cffe6be5df
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-10-20 19:49:19 +0300

    svgs + file loaders, Button Ui kit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNmes"
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 
 interface NavbarProps {
@@ -10,6 +11,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Main</AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>About</AppLink>
