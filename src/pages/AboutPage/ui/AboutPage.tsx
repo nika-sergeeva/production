1b3696a171c0d7f9ef20d04adeef352076dabb00
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2023-11-02 21:23:18 +0300

    jest added

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import i18n from 'shared/config/i18n/i18n';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
     return (
         <div>
+            {t('new key')}
             {t('О сайте')}
         </div>
     );
