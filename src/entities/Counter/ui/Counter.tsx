63bddd35d0ad90b431a9f9eadeba4d7a94ae0b8e
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2024-02-12 23:46:52 +0300

    redux-toolkit added

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'shared/ui/Button/Button';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector((state: StateSchema) => state.counter.value);
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div>
+            <h1>{value}</h1>
+            <Button 
+                onClick={increment}>
+                increment
+            </Button>
+            <Button 
+                onClick={decrement}>
+                decrement
+            </Button>
+        </div>
+    );
+};
