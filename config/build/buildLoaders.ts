e8c53d53d19a230bd4fad5d2d1bf90491c8898cb
Veronika Sergeeva <veroonika.sergeeva@gmail.com>
2024-04-02 22:19:42 +0300

    React refresh plugin. build babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,32 +1,17 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { buildBabelLoader } from './loaders/buildBabel.loader';
 
-export function buildLoaders({ isDev }:BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options:BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true,
-                        },
-                    ],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -28,6 +28,7 @@ export function buildPlugins({
     ];
 
     if (isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin());
         plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
